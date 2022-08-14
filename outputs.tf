98d9f78bea40d91dabbb0bb4c076d17fda4fb6a9
Matthew <matt@souldo.net>
2022-08-14 08:01:03 -0500

    fixes

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "resource_group_location" {
     value = azurerm_resource_group.rg.location
 }
 
-output "azure_ssh_public_key" {
-    value = var.azure_ssh_public_key
-}
-
 output "subnet_id" {
     value = azurerm_subnet.subnet.id
 }
